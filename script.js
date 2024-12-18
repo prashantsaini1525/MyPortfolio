cc3948fa015794999cdc338260cd958f3598c56e
PRASHANT SAINI <Prashant.saini.3150@gmail.com>
2024-12-18 20:41:21 +0530

    scroll

--- script.js
+++ script.js
@@ -9,18 +9,17 @@ window.addEventListener("scroll", function(){
 
 /*========================Scroll Section bottom to top====================*/
 document.addEventListener("DOMContentLoaded", function() {
-    $(document).ready(function(){
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 40) {
+            $('.gotopbtn a').fadeIn();
+        } else {
+            $('.gotopbtn a').fadeOut();
+        }
+    });
 
-        $(window).scroll(function(){
-            if($(this).scrollTop() > 40){
-                $('.gotopbtn a').fadeIn();
-            } else{
-                $('.gotopbtn a').fadeOut();
-            }
-        });
-    
-        $(".gotopbtn a").click(function(){
-            $('html, body').animate({scrollTop : 0},800);
-        });
-    }); 
+    $('.gotopbtn a').on('click', function(event) {
+        event.preventDefault();
+        $('html, body').animate({ scrollTop: 0 }, 800);
+    });
 });
+
