3a3a72429b1ddff107a9e9834a653bc2499433e6
PRASHANT SAINI <Prashant.saini.3150@gmail.com>
2024-12-18 14:09:30 +0530

    My Portfolio

---
+++ script.js
@@ -0,0 +1,24 @@
+/*==========================sticky nav============================*/
+
+window.addEventListener("scroll", function(){
+    var nav = document.querySelector("nav");
+    nav.classList.toggle("sticky", window.scrollY > 0);
+});
+
+
+
+/*========================Scroll Section bottom to top====================*/
+$(document).ready(function(){
+
+    $(window).scroll(function(){
+        if($(this).scrollTop() > 40){
+            $('.gotopbtn a').fadeIn();
+        } else{
+            $('.gotopbtn a').fadeOut();
+        }
+    });
+
+    $(".gotopbtn a").click(function(){
+        $('html, body').animate({scrollTop : 0},800);
+    });
+}); 
